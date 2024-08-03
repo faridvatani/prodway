852304969b0cf986c7a47380bb7d9cc85587aa5e
Farid Vatani <faridvatani@gmail.com>
2024-08-03 14:07:58 +0330

    feat: replace favicon with custom logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
 export { default as ArrowRight } from "@/src/assets/arrow-right.svg";
-export { default as Logo } from "@/src/assets/logosaas.png";
+export { default as Logo } from "@/src/assets/logo.png";
 export { default as Menu } from "@/src/assets/menu.svg";
 export { default as CogImage } from "@/src/assets/cog.png";
 export { default as CylinderImage } from "@/src/assets/cylinder.png";
